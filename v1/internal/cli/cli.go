9d9e43f36cf754c433375765b1f2e1d6aa7cdac4
Eiichi Sayama <eiichimouse@outlook.jp>
2026-06-27 17:08:49 +0900

    refactor:refine English
    For readability separate prompt templates from PromptBuil.d

--- v1/internal/cli/cli.go
+++ v1/internal/cli/cli.go
@@ -38,9 +38,7 @@ type PromptData struct {
 	OutputTemplate string
 }
 
-// For now use string, use template package when sending the request
 func PromptBuild() (string, error) {
-	const userInput = `git commit -m "{{.Title}}" -m "{{.Body}}"`
 
 	input, err := template.New("input").Parse(userInput)
 	if err != nil {
@@ -57,16 +55,12 @@ func PromptBuild() (string, error) {
 	}
 	s := inputBuf.String()
 
-	const basePrompt = "You are experienced software engineer. Review below git commit message draft from non native English speakers. In order to grow up strong point, keep the orignal message as much as possible. Reflect user's original structure of message as you can, too. Populate reviewed version into next template for output."
-	const outputTemplate = " - Sounds native English speaker version : \n - More precise and concise version : \n - Guessed missing context version : "
-
 	data := PromptData{
 		BasePrompt:     basePrompt,
 		OutputTemplate: outputTemplate,
 		UserInput:      s,
 	}
 
-	const prompTemplate = "Main instruction:\n  {{.BasePrompt}}\nUser input:\n{{.UserInput}}\nOutput templates:\n{{.OutputTemplate}}"
 	prompt, err := template.New("prompt").Parse(prompTemplate)
 	if err != nil {
 		return "", err
