165f594cc50b5e099967efc3e4a4dd852b24e9e7
Eiichi Sayama <eiichimouse@outlook.jp>
2026-08-15 17:42:57 +0900

    feat: add both changes in the staging area and all content of files with changes to AI request
    Focusing on returning changes as a string.

--- v2/internal/prompt/git.go
+++ v2/internal/prompt/git.go
@@ -2,7 +2,10 @@ package prompt
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func readGitLog() (string, error) {
@@ -13,4 +16,79 @@ func readGitLog() (string, error) {
 		fmt.Println("could not run command: ", err)
 	}
 	return string(out), nil
+}
+
+type Diff struct {
+	Diff         string
+	ChangedFiles []ChangedFile
+}
+
+type ChangedFile struct {
+	Path     string
+	AllLines string
+}
+
+func readChangesInStagedFile() (string, error) {
+	cmd := exec.Command("git", "diff", "--staged")
+
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func findChangedFiles() ([]string, error) {
+	cmd := exec.Command("git", "diff", "--staged", "--name-only")
+
+	out, err := cmd.Output()
+	if err != nil {
+		return []string{}, err
+	}
+
+	files := strings.Split(strings.ReplaceAll(string(out), "\r\n", "\n"), "\n")
+	files = files[:len(files)-1] //Trim last empty line
+
+	return files, nil
+}
+
+func getGitRootPath() (string, error) {
+	cmd := exec.Command("git", "rev-parse", "--show-toplevel")
+
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	rootPath := strings.Split(strings.ReplaceAll(string(out), "\r\n", "\n"), "\n")
+
+	return rootPath[0], nil
+
+}
+
+func buildDiff() (Diff, error) {
+	diff, err := readChangesInStagedFile()
+	if err != nil {
+		return Diff{}, err
+	}
+
+	changedFilesPath, err := findChangedFiles()
+	if err != nil {
+		return Diff{}, err
+	}
+
+	var changedFiles []ChangedFile
+	for _, v := range changedFilesPath {
+		gitRoot, err := getGitRootPath()
+		if err != nil {
+			fmt.Println("Not Found Current Directory")
+		}
+		path := filepath.Join(gitRoot, v)
+		content, err := os.ReadFile(path)
+		if err != nil {
+			fmt.Println("Not Found : ", path)
+		}
+		changedFiles = append(changedFiles, ChangedFile{Path: v, AllLines: string(content)})
+	}
+	return Diff{Diff: diff, ChangedFiles: changedFiles}, nil
 }
