12500867bf1a64734eaa9e99b113d32fc418b585
Eiichi Sayama <eiichimouse@outlook.jp>
2026-09-09 00:20:33 +0900

    test: test GitReviewInterpreter.Interpret accepts one m flag style commit message

--- v2/internal/cli/cli_test.go
+++ v2/internal/cli/cli_test.go
@@ -13,16 +13,14 @@ type InterpretCase struct {
 var ok1 = InterpretCase{
 	input: []string{"git-review", "-m", `test-title:
 
-first line of message body
-second line of message body
-third line of message body`},
+first line of message body.
+second line of message body.
+third line of message body.`},
 	expect: Command{
 		Instruction: "git-review",
 		Options: []Option{
 			{Flag: "-m", Value: "test-title:"},
-			{Flag: "-m", Value: `first line of message body
-second line of message body
-third line of message body`},
+			{Flag: "-m", Value: `first line of message body. second line of message body. third line of message body. `},
 		}},
 }
 
@@ -31,6 +29,16 @@ func TestGitReviewInterpreterInterpret(t *testing.T) {
 	command, err := gitReviewInterpreter.Interpret(ok1.input)
 	if err != nil {
 		t.Errorf("Failed to interpret : %d", err)
+	}
+	if command.Instruction != ok1.expect.Instruction {
+		t.Errorf("Failed to parse main command.")
+	}
+	if command.Options[0].Flag != ok1.expect.Options[0].Flag ||
+		command.Options[0].Value != ok1.expect.Options[0].Value ||
+		command.Options[1].Flag != ok1.expect.Options[1].Flag ||
+		command.Options[1].Value != ok1.expect.Options[1].Value {
+
+		t.Errorf("Failed to parse options.")
 	}
 	fmt.Println("command : ", command)
 
